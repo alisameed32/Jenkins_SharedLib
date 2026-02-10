589d8ba585ad2bbee2c4f3410cb7abe9c4c438bc
Ali Sameed <alisameed.cs@gmail.com>
2026-02-10 05:51:10 +0500

    Refactor owasp_dependency function to remove NVD API Key argument for simplified usage

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,8 +1,4 @@
-// 1. Add 'String nvdKey' inside the parenthesis to accept the argument
-def call(String nvdKey){
-    
-    // 2. Use the variable '${nvdKey}' in the additionalArguments
-    dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${nvdKey}", odcInstallation: 'OWASP'
-    
-    dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
+def call(){
+  dependencyCheck additionalArguments: '--scan ./', odcInstallation: 'OWASP'
+  dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
