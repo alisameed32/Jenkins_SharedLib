09bb7697a76d0d2e4a42860f74e3749f91725b33
Ali Sameed <alisameed.cs@gmail.com>
2026-02-10 05:41:07 +0500

    Refactor owasp_dependency function to remove '--purge' option for cleaner database handling

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,6 +1,8 @@
+// 1. Add 'String nvdKey' inside the parenthesis to accept the argument
 def call(String nvdKey){
-    // Added '--purge' to delete the corrupted database
-    dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${nvdKey} --purge", odcInstallation: 'OWASP'
+    
+    // 2. Use the variable '${nvdKey}' in the additionalArguments
+    dependencyCheck additionalArguments: "--scan ./ --nvdApiKey ${nvdKey}", odcInstallation: 'OWASP'
     
     dependencyCheckPublisher pattern: '**/dependency-check-report.xml'
 }
